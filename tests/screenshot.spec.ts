02c22de5bfeb6eeb94530a8cfb55564a4a971cbd
venukailash <venukailash>
2025-03-19 22:17:12 +0000

    initial commit

---
+++ tests/screenshot.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('has image screenshot', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+
+  const imgLocator = page.getByAltText('Browsers (Chromium, Firefox, WebKit)');
+  await expect(imgLocator).toBeVisible();
+  // await expect(imgLocator).toHaveScreenshot();
+});
+
